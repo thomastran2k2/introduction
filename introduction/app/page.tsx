173493e47e4b00801516f7a52e4140a3345f6a63
Thomas <thomastran1911@gmail.com>
2025-05-14 07:34:52 +1000

    created new logo

--- introduction/app/page.tsx
+++ introduction/app/page.tsx
@@ -17,12 +17,12 @@ export default function Home() {
         
         <div className="flex-1">
         <p className={`text-white rounded-lg  text-lg text-left`}>
-          Hi, I'm Thomas Tran. I am a data analyst based in Darwin, NT. I have a passion for analysing data and web development. I love analysing data and using it to tell stories. In my free time, I enjoy learning new technologies and improving my skills.
+          Hi, I&apos;m Thomas Tran. I am a data analyst based in Darwin, NT. I have a passion for analysing data and web development. I love analysing data and using it to tell stories. In my free time, I enjoy learning new technologies and improving my skills.
         </p>
         <div className="flex justify-start mt-4">
           <a 
           href="/projects"
-          className="bg-green-400 text-white px-6 py-3  hover:bg-green-600 transition text-lg"
+          className="bg-green-400 text-white px-6 py-3  hover:bg-green-600 transition text-lg rounded-lg"
           >
           See My Projects
           </a>
@@ -67,6 +67,13 @@ export default function Home() {
         <p className="text-lg text-black ">Bachelor of Computer Science </p>
         <p className="text-lg text-black ">Bachelor of Commerce</p>
         </div>
+      </div>
+      
+      </section>
+      <section className="mb-8 bg-gray-100 text-center py-4 w-screen">
+      <h2 className="text-2xl font-bold mb-4 text-center text-black">Technology</h2>
+      <div className="flex space-x-8 justify-center items-center  py-4">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 98 24"  width="82"><g fill="currentColor"><path d="M36.82 12.02c0-2.97-.85-4.5-2.56-4.58a4.82 4.82 0 0 0-1.98.33 2.9 2.9 0 0 0-1.04.54v7.1c1.09.68 2.06 1 2.9.94 1.78-.11 2.68-1.56 2.68-4.33zm2.1.13c0 1.5-.36 2.76-1.07 3.76a3.96 3.96 0 0 1-3.32 1.74 5.9 5.9 0 0 1-3.29-1v6.45l-1.83-.65V8.15c.3-.37.69-.69 1.16-.96a7.95 7.95 0 0 1 3.96-.97l.03.03A3.64 3.64 0 0 1 37.84 8a7.37 7.37 0 0 1 1.08 4.16zm11.16 4.23c0 2.02-.2 3.42-.61 4.2a4.3 4.3 0 0 1-2.34 1.87 9 9 0 0 1-3.03.6l-.3-1.15c1.1-.15 1.88-.3 2.32-.44.89-.3 1.5-.76 1.83-1.37.27-.5.4-1.46.4-2.87v-.47a9.42 9.42 0 0 1-3.93.85c-.9 0-1.69-.28-2.37-.85a2.87 2.87 0 0 1-1.15-2.34V6.85l1.82-.62v7.61c0 .81.27 1.44.8 1.88a3 3 0 0 0 2.04.63 5.14 5.14 0 0 0 2.69-.97V6.5h1.83v9.88zm7.13 1.17a7.34 7.34 0 0 1-.6.02 3.6 3.6 0 0 1-2.42-.73 2.54 2.54 0 0 1-.86-2.04V7.64h-1.25V6.5h1.25V3.46l1.82-.64V6.5h2.06v1.14h-2.06v7.11c0 .69.19 1.17.55 1.45.32.23.82.37 1.5.4v.95zm11.05-.15h-1.82v-7.02a3.3 3.3 0 0 0-.5-1.84 1.83 1.83 0 0 0-1.61-.87c-.84 0-1.89.44-3.14 1.32v8.4h-1.83V.64l1.83-.58V7.7A6.38 6.38 0 0 1 65 6.42c.97 0 1.75.33 2.35.97.6.65.9 1.46.9 2.42v7.59zm9.7-5.66c0-1.14-.21-2.09-.64-2.83a2.73 2.73 0 0 0-2.4-1.44c-2 .12-3 1.54-3 4.28a7 7 0 0 0 .62 3.14c.54 1.08 1.34 1.6 2.41 1.59 2.01-.02 3.02-1.6 3.02-4.74zm2 .01c0 1.62-.41 2.98-1.24 4.06a4.5 4.5 0 0 1-3.8 1.81c-1.6 0-2.85-.6-3.76-1.81a6.53 6.53 0 0 1-1.22-4.06c0-1.53.44-2.81 1.32-3.86a4.57 4.57 0 0 1 3.68-1.67c1.52 0 2.75.56 3.7 1.67a5.78 5.78 0 0 1 1.33 3.86zm10.48 5.65H88.6V9.98c0-.81-.25-1.44-.74-1.9a2.69 2.69 0 0 0-1.96-.66c-.87.02-1.7.3-2.48.84v9.14h-1.82V8.04a9.03 9.03 0 0 1 2.9-1.5 8.87 8.87 0 0 1 2.2-.32c.43 0 .84.05 1.22.13.72.17 1.3.47 1.75.92.5.5.76 1.1.76 1.8v8.33zM11.38.7c-.94 0-1.84.08-2.63.22C6.43 1.32 6 2.18 6 3.76v2.09h5.5v.69H3.94c-1.6 0-3 .96-3.43 2.77-.5 2.09-.53 3.39 0 5.56.39 1.62 1.32 2.78 2.92 2.78h1.89v-2.5c0-1.8 1.57-3.4 3.43-3.4h5.5a2.76 2.76 0 0 0 2.74-2.78V3.76c0-1.48-1.26-2.6-2.75-2.84a17.22 17.22 0 0 0-2.86-.23zM8.4 2.36a1.04 1.04 0 0 1 1.03 1.05c0 .57-.47 1.04-1.03 1.04-.57 0-1.03-.47-1.03-1.04 0-.58.46-1.05 1.03-1.05z"></path><path d="M17.68 6.54v2.43c0 1.88-1.6 3.47-3.44 3.47H8.75A2.8 2.8 0 0 0 6 15.22v5.2c0 1.5 1.3 2.36 2.75 2.79 1.74.5 3.41.6 5.5 0 1.37-.4 2.74-1.2 2.74-2.78v-2.09H11.5v-.7h8.24c1.6 0 2.2-1.1 2.75-2.77.57-1.7.55-3.36 0-5.56-.4-1.58-1.15-2.77-2.75-2.77h-2.06zm-3.1 13.2a1.04 1.04 0 0 1 0 2.08 1.04 1.04 0 0 1-1.02-1.05c0-.57.46-1.04 1.03-1.04zM94 4.87h.32v-2h.76v-.24h-1.84v.24H94v2m1.36 0h.28V3.05l.6 1.83h.3l.61-1.82v1.82h.3V2.64h-.4l-.67 1.92-.57-1.92h-.45v2.24"></path></g></svg>
       </div>
       
       </section>
--- introduction/components/NavBar.tsx
+++ introduction/components/NavBar.tsx
@@ -1,25 +1,39 @@
 import React from 'react'
+import Link from 'next/link'
+import {Playwrite_DK_Loopet } from "next/font/google"
 
+const playwrite = Playwrite_DK_Loopet({
+  
+  weight: ["100", "200", "300", "400"],
+})
 const NavBar = () => {
   return (
     <div>
-      <nav className="flex justify-between p-4 bg-gray-100">
-        <div className=" text-black font-bold text-xl">Introduction page</div>
+      <nav className="flex justify-between p-4 bg-zinc-800">
+        <div
+          className={`text-black font-bold text-xl ${playwrite.className}`}
+          
+        >
+          <Link href="/" className='header'>Thomas Tran</Link>
+          
+        </div>
         <ul className="flex list-none gap-4 m-0 p-0">
-          <li className="group">
-            <a
+          <li className='group'>
+            <Link
               href="/"
-              className="relative px-4 py-2 text-black cursor-pointer transition-opacity"
+              className="relative px-4 py-2 text-white cursor-pointer transition-opacity"
             >
+            
               <span>Home</span>
               <span className="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
               <span className="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
-            </a>
+            </Link>
+            
           </li>
           <li className="group">
             <a
               href="/projects"
-              className="relative px-4 py-2 text-black cursor-pointer transition-opacity"
+              className="relative px-4 py-2 text-white cursor-pointer transition-opacity"
             >
               <span>Projects</span>
               <span className="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
@@ -29,7 +43,7 @@ const NavBar = () => {
           <li className="group">
             <a
               href="/contact"
-              className="relative px-4 py-2 text-black cursor-pointer transition-opacity"
+              className="relative px-4 py-2 text-white cursor-pointer transition-opacity"
             >
               <span>Contact</span>
               <span className="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-indigo-600 group-hover:w-3/6"></span>
